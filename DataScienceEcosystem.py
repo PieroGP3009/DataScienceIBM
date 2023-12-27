8df656558ab76c785eed8cd9442cf5eded08cee5
Piero Guillen <135992649+PieroGP3009@users.noreply.github.com>
2023-12-27 14:27:16 -0400

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# 1. Python: Widely recognized for its simplicity and efficiency, Python boasts an extensive array of libraries such as Pandas, NumPy, SciPy, Scikit-learn, and TensorFlow for data manipulation, statistical analysis, and machine learning.
+# 2. R: Particularly popular for statistical analysis and data visualization, R is highly regarded for its ability to handle tabular data, arrays, and perform complex statistical operations.
+# 3. SQL (Structured Query Language): Essential for working with relational databases, SQL enables the extraction, manipulation, and analysis of large data sets stored in databases.
+# 4. Java: While not as common as Python or R for data analytics, it's used in certain data science areas, especially in environments that require integration with Java-based applications or for large-scale systems.
+# 5. Scala: Mainly used in big data environments, especially with Apache Spark, a popular tool for processing large data sets. Scala combines the functionality of object-oriented and functional languages.
+
+# **Objectives:**
+# - Understand and apply basic arithmetic expressions in Python.
+# - Identify popular languages for Data Science, including Python and R.
+# - Learn to use key Python libraries such as Pandas, NumPy, and Matplotlib for data analysis and visualization.
+# - Familiarize with the functionality and usage of Jupyter Notebooks in data science workflows.
+# - Explore the concepts of version control and the usage of GitHub in managing data science projects.
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Pandas: A foundational library for data manipulation and analysis in Python. Pandas provide easy-to-use data structures and data analysis tools, making it ideal for tasks like data cleaning, transformation, and aggregation.
+# 2. NumPy: Essential for scientific computing in Python, NumPy offers support for large, multi-dimensional arrays and matrices, along with a collection of mathematical functions to operate on these arrays. It's widely used for numerical data processing.
+# 3. Scikit-learn: A key library for machine learning in Python. Scikit-learn provides a range of supervised and unsupervised learning algorithms via a consistent interface. It's known for its simplicity in implementing complex machine learning models.
+
+# | Data Science Tools | 
+# |--------------------|
+# |Jupyter Notebook| 
+# | RStudio |
+# | Apache Zeppelin |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# ###### Addition
+# 3 + 5
+# ###### Subtraction
+# 10 - 4
+# ###### Multiplication
+# 6 * 7
+# ###### Division
+# 20 / 4
+
+# In[1]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[3]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+print('200 minutes is equal to', 200 / 60, 'hours')
+
+
+# ## Author ## 
+# Piero Guillen 
